c84bb2f57b3b9c4f1b9366a9bcf7f536d135cd15
AdityaS1426 <71394490+AdityaS1426@users.noreply.github.com>
2021-05-20 08:09:38 -0700

    Initial commit

---
+++ src/main/java/com/booleans/calendartest/CalendarTestApplication.java
@@ -0,0 +1,12 @@
+package com.booleans.calendartest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarTestApplication.class, args);
+    }
+}
