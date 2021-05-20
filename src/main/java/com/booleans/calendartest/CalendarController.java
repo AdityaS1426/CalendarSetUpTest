208a9f47609f51655c0d1c0fef5a5fed38aa9790
AdityaS1426 <71394490+AdityaS1426@users.noreply.github.com>
2021-05-20 08:39:58 -0700

    Updates.

--- src/main/java/com/booleans/calendartest/CalendarController.java
+++ src/main/java/com/booleans/calendartest/CalendarController.java
@@ -14,11 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- *
- * @author rutpatel
- *
- */
 @Controller
 public class CalendarController {
     HashMap<String, ArrayList<Date>> calendar = new HashMap<>();
