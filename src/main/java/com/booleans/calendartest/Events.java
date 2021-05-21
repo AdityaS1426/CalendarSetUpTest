cdad887c40971a93ea130cf078e6861a6ac66315
YasaswiS <49824724+YasaswiS@users.noreply.github.com>
2021-05-20 23:07:04 -0700

    Create Events.java

---
+++ src/main/java/com/booleans/calendartest/Events.java
@@ -0,0 +1,13 @@
+// @Entity
+// @Table(name = "Events")
+// public class Events {
+//     @Id
+//     Long id;
+//     @Column(name = "TITLE")
+//     String title;
+
+//     @Column(name = "DESCRIPTION")
+//     String description;
+    
+//     // getters & setters
+// }
